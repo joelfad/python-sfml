1da38fc5e5f4b2eedd3ccfaac8b3ba0d7de3787e
Gaulois94 <serenomickael@gmail.com>
2016-01-08 23:53:05 +0000

    Add hacks.cpp

---
+++ src/sfml/hacks.cpp
@@ -0,0 +1,26 @@
+#include "hacks.h"
+
+sf::Time Time_div_int(sf::Time left, sf::Int64 right)
+{
+	return left / right;
+}
+
+sf::Time Time_div_float(sf::Time left, float right)
+{
+	return left / right;
+}
+
+float Time_div_Time(sf::Time left, sf::Time right)
+{
+	return left / right;
+}
+
+void Time_idiv_int(sf::Time& left, sf::Int64 right)
+{
+	left / right;
+}
+
+void Time_idiv_float(sf::Time& left, float right)
+{
+	left / right;
+}
